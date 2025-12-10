c6b720db49c661632ea99a596a2407da8f9942ae
Rijul Rangta <rijulrangta@gmail.com>
2025-12-10 19:30:01 +0530

    fixed scaling issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,9 @@
-"use client";
-
-import dynamic from "next/dynamic";
 import MainPage from "@/components/MainPage";
 
 import Gallery from "@/components/Gallery";
 import MissionBriefing from "@/components/MissionBriefing";
 
-const Themes = dynamic(
-  () => import("@/components/ThemesSection/ThemesSection"),
-  { ssr: false }
-);
+import Themes from "@/components/ThemesSection/ThemesSection";
 
 import Testimonials from "@/components/Testimonials/Testimonials";
 export default function Page() {
