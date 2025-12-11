81774aa540354aac9bd91df058f7e5793ee44727
ArchitaAgrawal912 <24bec024@nith.ac.in>
2025-12-11 22:02:33 +0530

    Changed Gallery component

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,17 @@
 import MainPage from "@/components/MainPage";
-import Gallery from "@/components/Gallery";
 import MissionBriefing from "@/components/MissionBriefing";
 import Themes from "@/components/ThemesSection/ThemesSection";
 import Testimonials from "@/components/Testimonials/Testimonials";
 import Timeline from "@/components/Timeline/timeline";
-
+import GalleryPage from "@/components/gallery/gallery";
 export default function Page() {
   return (
     <>
       <MainPage />
       <MissionBriefing />
-      <Gallery />
       <Themes />
-      <Timeline />        
+      <Timeline /> 
+      <GalleryPage />       
       <Testimonials />
     </>
   );
