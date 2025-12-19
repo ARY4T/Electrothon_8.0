c1c5fbca8c1f7c972d36abbfafce1f4731466764
ArchitaAgrawal912 <24bec024@nith.ac.in>
2025-12-19 12:46:39 +0530

    Added loader

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Preloader from "@/components/preloader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,10 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
         suppressHydrationWarning
       >
-        {children}
+       
+        <Preloader>
+          {children}
+        </Preloader>
       </body>
     </html>
   );
