9274c7a0ad1cdedacd1244c76f9707f5927d01e1
Vanya Sharma <vanya2007sharma@gmail.com>
2025-12-27 17:53:35 +0530

    Refine Mission Briefing layout and achievements responsiveness

--- src/components/MainPage/Countdown.jsx
+++ src/components/MainPage/Countdown.jsx
@@ -42,9 +42,10 @@ export default function Countdown({ targetDate = "2025-03-01T23:59:59" }) {
   }, [targetDate]);
 
   return (
+    
     <div
       className="
-    absolute bottom-[10vh] left-1/2
+    absolute bottom-[6vh] left-1/2
     -translate-x-1/2
     z-20
     origin-bottom
